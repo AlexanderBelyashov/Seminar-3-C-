78c7f7af2a8281018ec20f8b75d8e931b1cfbd09
Hunter <alexanderbelyashov@yandex.ru>
2022-10-14 15:57:36 +0300

    Seminar 3 C#

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
+*/
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Ввод данных
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
